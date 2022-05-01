bdda0aa75801d3efe8dbcd65f50cda51bcbd0f2e
roj21008 <roj21008@byui.edu>
2022-04-30 20:44:47 -0500

    challenges

---
+++ week2/ch-3.js
@@ -0,0 +1,19 @@
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+let score = 0;
+
+for (const[question,answer] of quiz){
+    const response = prompt(question);
+    if(response == answer){
+        alert('correct');
+        score++;
+    }else{
+        alert(`Wrong, The correct answer is: " ${answer}"`)
+    }
+
+}
+
+alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
